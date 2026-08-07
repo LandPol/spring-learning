d03971d7e54770e202fa3b350f7cf8aa0668dbd2
LandPol <mateuszkacprzak@op.pl>
2026-08-08 01:23:09 +0200

    Added global exception handling for missing tasks

---
+++ spring-learning/src/main/java/com/example/springlearning/spring_learning/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.example.springlearning.spring_learning.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException e) {
+        return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
