1cc30cc9069c5241b4ebbf7c914c2a8a189baa84
LandPol <mateuszkacprzak@op.pl>
2026-08-23 01:06:08 +0200

    Added tests for addNewTask

--- spring-learning/src/test/java/com/example/springlearning/spring_learning/service/TaskServiceTest.java
+++ spring-learning/src/test/java/com/example/springlearning/spring_learning/service/TaskServiceTest.java
@@ -1,5 +1,7 @@
 package com.example.springlearning.spring_learning.service;
 
+import com.example.springlearning.spring_learning.dto.CreateTaskRequest;
+import com.example.springlearning.spring_learning.exception.TaskAlreadyExistsException;
 import com.example.springlearning.spring_learning.exception.TaskNotFoundException;
 import com.example.springlearning.spring_learning.model.Task;
 import com.example.springlearning.spring_learning.repository.TaskRepository;
@@ -13,7 +15,8 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class TaskServiceTest {
@@ -40,5 +43,29 @@ public class TaskServiceTest {
         TaskNotFoundException exception = assertThrows(TaskNotFoundException.class, () -> taskService.getTaskById(1L));
 
         assertEquals("Task not found.", exception.getMessage());
+    }
+
+    @Test
+    void shouldReturnTaskWhenTaskAddedSuccessfully() {
+        CreateTaskRequest createTaskRequest = new CreateTaskRequest("Task 1", "Description 1", 3);
+        when(taskRepository.existsByTitle(createTaskRequest.getTitle())).thenReturn(false);
+
+        Task task = new Task(1L, "Task 1", "Description 1", 3);
+        when(taskRepository.save(any(Task.class))).thenReturn(task);
+
+        Task taskResult = taskService.addNewTask(createTaskRequest);
+
+        verify(taskRepository).save(any(Task.class));
+        assertEquals(task, taskResult);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenTaskAlreadyExists() {
+        CreateTaskRequest createTaskRequest = new CreateTaskRequest("Task 1", "Description 1", 3);
+        when(taskRepository.existsByTitle(createTaskRequest.getTitle())).thenReturn(true);
+
+        TaskAlreadyExistsException exception = assertThrows(TaskAlreadyExistsException.class, () -> taskService.addNewTask(createTaskRequest));
+        verify(taskRepository, never()).save(any(Task.class));
+        assertEquals("Task already exists", exception.getMessage());
     }
 }
